10e894c013d634db833295118258f80f76738cef
Yahor Piskunou <egor.piskunou@gmail.com>
2023-05-17 20:04:52 +0200

    refact app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+import { GoogleMapsService } from './google-maps/google-maps.service';
+import { GoogleMapsModule } from './google-maps/google-maps.module';
+import { WeatherModule } from './weather/weather.module';
+import { OpenweathermapService } from './openweathermap/openweathermap.service';
+import { FavoritePlaceModule } from './favorite-place/favorite-place.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot(),
+    WeatherModule,
+    FavoritePlaceModule,
+    GoogleMapsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api/(.*)'],
+    }),
+  ],
+  providers: [GoogleMapsService, OpenweathermapService],
 })
 export class AppModule {}
